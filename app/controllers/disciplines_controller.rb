13553f28a6e08ca78f172e55c318fa68dea40464
Maciorex <maciej.zabski@gmail.com>
2018-03-10 17:02:09 +0100

    add form for new disciplines and create action

--- app/controllers/disciplines_controller.rb
+++ app/controllers/disciplines_controller.rb
@@ -3,7 +3,23 @@ class DisciplinesController < ApplicationController
     @disciplines = Discipline.all
   end
 
-  def new; end
+  def new
+    @discipline = Discipline.new
+  end
 
-  def create; end
+  def create
+    @discipline = Discipline.new(discipline_params)
+    if @discipline.save
+      flash[:success] = "ŻEM DODOŁ DYSCYPLINA"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def discipline_params
+    params.require(:discipline).permit(:name, :description)
+  end
+
 end
