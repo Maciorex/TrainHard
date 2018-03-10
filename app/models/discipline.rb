8016579f82f0e9871c06c252da7616f9458dd746
Maciorex <maciej.zabski@gmail.com>
2018-03-10 17:11:09 +0100

    delete wrong relation

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -1,5 +1,4 @@
 class Discipline < ApplicationRecord
-  belongs_to :user
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true, length: {minimum: 10, maximum: 300}
 end
