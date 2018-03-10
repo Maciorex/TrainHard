a4df6a4dc68d6fbe1b4a829fc3fa32a320884bf7
Maciorex <maciej.zabski@gmail.com>
2018-03-10 16:34:52 +0100

    add validations to disciplines and relations

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -1,3 +1,5 @@
 class Discipline < ApplicationRecord
-
+  belongs_to :user
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
 end
