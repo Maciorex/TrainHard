4b2db33d72fbe60e7059df9d63bc872955d57023
DejwS <dawid@gryfen.pl>
2018-03-11 16:24:49 +0100

    correct My Training menu - current user check

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,9 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20180311131635) do
+
   create_table "disciplines", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
