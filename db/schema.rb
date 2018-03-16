180cb5a7de9c123d63a20bb2f64898a515e68fec
Maciorex <maciej.zabski@gmail.com>
2018-03-16 17:56:48 +0100

    add full name to users and method. Add login and logout buttons to navigations partial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180311131635) do
+ActiveRecord::Schema.define(version: 20180316162927) do
 
   create_table "disciplines", force: :cascade do |t|
     t.string "name"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20180311131635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "first_name"
+    t.string "second_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
