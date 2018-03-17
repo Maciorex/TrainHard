69108f5471d1c5fc0314716104784bd540a297da
Maciorex <maciej.zabski@gmail.com>
2018-03-17 12:45:45 +0100

    add training search form

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,7 +2,7 @@ class UserDecorator < Draper::Decorator
   delegate_all
 
   def full_name
-    return "Anonymous" unless object.first_name || object.last_name
-    "#{object.first_name} #{object.last_name}"
+    return "Anonymous" unless object.first_name || object.second_name
+    "#{object.first_name} #{object.second_name}"
   end
 end
