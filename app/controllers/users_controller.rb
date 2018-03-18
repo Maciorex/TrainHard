8a94fb2541a0060e1de670ead9558ca68ca10791
Maciorex <maciej.zabski@gmail.com>
2018-03-18 12:44:36 +0100

    Add elapsed time statistic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 
   def my_profile
-
+    @time = 0
+    current_user.trainings.each do |training|
+      @time += training.time_in_minutes
+    end
   end
 end
