cf2bc21ba28c183b683131c3c7ed1e56811ad7be
Maciorex <maciej.zabski@gmail.com>
2018-03-18 11:48:19 +0100

    Add my_profile page

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,9 @@
+class FriendshipsController < ApplicationController
+
+  def add_friend
+
+  end
+  def destroy
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317164644) do
+ActiveRecord::Schema.define(version: 20180318095048) do
 
   create_table "disciplines", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "trainings", force: :cascade do |t|
