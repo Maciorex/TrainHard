24f111841cc475a1be08142035c3e56b8cc040c6
Maciorex <maciej.zabski@gmail.com>
2018-03-23 19:58:03 +0100

    Add new tests

--- spec/controllers/disciplines_controller_spec.rb
+++ spec/controllers/disciplines_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe DisciplinesController do
   let(:user) { User.create(email: "ecik@janoszka.pl", password: "password") }
   let(:admin) { User.create(email: "podstawek@grzesio.pl", password: "password", admin: true)}
-  let(:discipline) { Discipline.create(name: "bierki") }
+  let(:discipline) { Discipline.create(name: "bierki", description: "mega jaja super") }
 
   describe '#index' do
     before { sign_in user }
@@ -31,6 +31,51 @@ describe DisciplinesController do
     it "returns 200" do
       subject
       expect(response.status).to be 200
+    end
+  end
+
+  describe "#create" do
+    subject { get :new }
+    before { sign_in admin }
+
+    it "instantiates Discipline" do
+      expect(Discipline).to receive(:new)
+      subject
+    end
+
+    it "returns 200" do
+      subject
+      expect(response.status).to be 200
+    end
+  end
+
+  describe "#show" do
+    before { sign_in user }
+    subject { get :show, params: { id: discipline.id } }
+
+    it "returns 200" do
+      subject
+      expect(response.status).to be 200
+    end
+
+    it "calls 'Discipline.find'" do
+      expect(Discipline).to receive(:find)
+      subject
+    end
+  end
+
+  describe "#destroy" do
+    before { sign_in admin }
+    subject { delete :destroy, params: { id: discipline.id } }
+
+    it "returns 302" do
+     subject
+     expect(response.status).to be 302
+    end
+
+    it "calls 'discipline.destroy'" do
+      subject
+      expect(Discipline.count).to eq(0)
     end
   end
 end
