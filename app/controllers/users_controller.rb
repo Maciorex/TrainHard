33fa2cec35b5ee367f79d0d2530aa2ba35c376c4
Maciorex <maciej.zabski@gmail.com>
2018-03-24 11:23:43 +0100

    update show users page and trainings search engine

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,14 @@ class UsersController < ApplicationController
   end
 
   def my_friends; end
+
+  def show
+    @user = User.find(params[:id])
+    @time = 0
+    @distance = 0
+    @user.trainings.each do |training|
+      @time += training.time_in_minutes
+      @distance += training.distance_meters
+    end
+  end
 end
